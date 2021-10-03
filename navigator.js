ce25c7d90379ca57bd75b2be97b45a1d65391e56
ssatguru <ssatguru@gmail.com>
2021-10-03 00:32:03 -0500

    initial checkin

---
+++ navigator.js
@@ -0,0 +1,48 @@
+document.write("<div class='container'>");
+document.write("	<div class='row'>");
+document.write("		<div class='span12 '>");
+document.write("			<div class='navbar'>");
+document.write("				<div class='navbar-inner'>");
+document.write("					<ul class='nav'>");
+document.write("						<li><a href='../home/index.html' target='_top'>Home</a></li>");
+
+document.write("						<li class='dropdown'>");
+document.write("  							<a id='unityLabel' class='dropdown-toggle' role='button' data-toggle='dropdown' href='#'>Unity3D<b class='caret'></b></a>");
+document.write("  							<ul class='dropdown-menu' role='menu' aria-labelledby='unityLabel'>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1' href='../Unity3D/main.html' target='_top'>Island</a> </li>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1' href='../yofrankie/yofrankie.html' target='_top'>Yo Frankie!</a> </li>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1' href='../Unity3D_AngryBots/angrybots.html' target='_top'>AngryBots</a> </li>");
+document.write("  							</ul>");
+document.write("						</li>");
+
+document.write("						<li><a href='../gwt/SpiroGraph.html' target='_top'>GWT fun</a></li>");
+document.write("						<li><a href='../Misc/main.html' target='_top'>Java Midi</a></li>");
+
+document.write("						<li class='dropdown'>");
+document.write("  							<a id='dLabel' class='dropdown-toggle' role='button' data-toggle='dropdown' href='#'>BabylonJS<b class='caret'></b></a>");
+document.write("  							<ul class='dropdown-menu' role='menu' aria-labelledby='dLabel'>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1'href='../BabylonJS-Vishva/intro.html' target='_top' >Vishva</a></li>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1'href='https://ssatguru.github.io/BabylonJS-EditControl-Samples/demo/Demo.html' target='link' >EditControl</a></li>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1'href='https://ssatguru.github.io/BabylonJS-CharacterController-Samples/demo/' target='link' >Character Controller</a></li>");
+document.write("  							</ul>");
+document.write("						</li>");
+
+document.write("						<li><a href='../LeapMotion/main.html' target='_top'>LeapMotion</a></li>");
+
+document.write("						<li class='dropdown'>");
+document.write("  							<a id='dLabel' class='dropdown-toggle' role='button' data-toggle='dropdown' href='#'>Archive<b class='caret'></b></a>");
+document.write("  							<ul class='dropdown-menu' role='menu' aria-labelledby='dLabel'>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1' href='../NetRexx/main.html' target='_top' >NetRexx</a></li>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1' href='../SQR/main.html' target='_top'>Sqr</a></li>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1' href='../SharpDevelop/main.html' target='_top'>SharpDevelop</a></li>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1' href='../CloudParty/cp_games.html' target='_top' >CloudParty Games</a></li>");
+document.write("								<li role='presentation'><a role='menuitem' tabindex='-1' href='../CloudParty/b2c.html' target='_top'>CloudParty Converter</a></li>");
+document.write("  							</ul>");
+document.write("						</li>");
+
+document.write("					</ul>");
+document.write("				</div>");
+document.write("			</div>");
+document.write("		</div>");
+document.write("	</div>");
+document.write("</div>");
